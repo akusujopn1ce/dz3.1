b1b2c37e7babe3a0fc6816dc7d759a27d40d65f2
akusujopn1ce <akusujopnice007@gmail.com>
2026-07-09 02:42:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-let userName = prompt("Введіть ваше ім'я:", "");
+let input = prompt("Введіть тризначне число:", "");
+let num = parseInt(input);
 
-if (userName !== null && userName !== "") {
-    alert(`Hello, ${userName}! How are you?`);
+if (!isNaN(num) && num >= 100 && num <= 999) {
+    
+    let d1 = Math.floor(num / 100);      
+    let d2 = Math.floor((num % 100) / 10); 
+    let d3 = num % 10;                     
+
+    let allSame = (d1 === d2 && d2 === d3);
+
+    let anySame = (d1 === d2 || d1 === d3 || d2 === d3);
+
+    alert(
+        `Ви ввели число: ${num}\n` +
+        `Всі цифри однакові: ${allSame ? "Так" : "Ні"}\n` +
+        `Є однакові цифри: ${anySame ? "Так" : "Ні"}`
+    );
+
 } else {
-    alert("Ви не ввели ім'я!");
+    alert("Помилка! Будь ласка, введіть саме тризначне число (від 100 до 999).");
 }
