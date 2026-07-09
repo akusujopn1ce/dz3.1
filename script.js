3c9c917b8024f93f5f2ad06d49570835fc1f707e
akusujopn1ce <akusujopnice007@gmail.com>
2026-07-09 03:11:14 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,17 @@
-const currentYear = new Date().getFullYear();
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
 
-let birthYear = prompt("Введіть рік вашого народження:", "");
-let city = prompt("В якому місті ви живете?", "");
-let sport = prompt("Ваш улюблений вид спорту?", "");
-
-let message = "";
-
-if (birthYear === null || birthYear.trim() === "") {
-    message += "Шкода, що Ви не захотіли ввести свою дату народження.\n";
-} else {
-    let age = currentYear - parseInt(birthYear);
-    message += `Ваш вік: ${age}.\n`;
+switch (true) {
+    case numOrStr === null:
+        console.log('ви скасували');
+        break;
+    case numOrStr.trim() === '':
+        console.log('Empty String');
+        break;
+        
+    case isNaN(+numOrStr):
+        console.log(' number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
 }
-
-if (city === null || city.trim() === "") {
-    message += "Шкода, що Ви не захотіли ввести своє місто.\n";
-} else {
-    let lowerCity = city.toLowerCase().trim();
-    
-    if (lowerCity === "київ") {
-        message += "Ти живеш у столиці України.\n";
-    } else if (lowerCity === "вашингтон") {
-        message += "Ти живеш у столиці США.\n";
-    } else if (lowerCity === "лондон") {
-        message += "Ти живеш у столиці Великої Британії.\n";
-    } else {
-        message += `Ти живеш у місті ${city}.\n`;
-    }
-}
-
-if (sport === null || sport.trim() === "") {
-    message += "Шкода, що Ви не захотіли ввести свій вид спорту.\n";
-} else {
-    let lowerSport = sport.toLowerCase().trim();
-    
-    if (lowerSport === "футбол") {
-        message += "Круто! Хочеш стати Ліонелем Мессі?\n";
-    } else if (lowerSport === "бокс") {
-        message += "Круто! Хочеш стати Олександром Усиком?\n";
-    } else if (lowerSport === "баскетбол") {
-        message += "Круто! Хочеш стати Майклом Джорданом?\n";
-    }
-}
-
-alert(message);
