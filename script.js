21d54939d5ad12f3dac373125a9a888928c02b71
akusujopn1ce <akusujopnice007@gmail.com>
2026-08-27 16:11:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-let ladder = {
-  step: 0,
-
-  up: function () {
-    this.step++;
-    return this;
-  },
-
-  down: function () {
-    this.step--;
-    return this; 
-  },
-
-  showStep: function () {
-    console.log(this.step); 
-    return this; 
+let company = {
+  sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 600}],
+  development: {
+    web: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800}],
+    internals: [{name: 'Jack', salary: 1300}]
   }
 };
 
-ladder.up().up().down().showStep();
+function sumSalaries(department) {
+  if (Array.isArray(department)) {
+    return department.reduce((sum, employee) => sum + employee.salary, 0);
+  } 
+  else {
+    let sum = 0;
+    for (let subdep of Object.values(department)) {
+      sum += sumSalaries(subdep);
+    }
+    return sum;
+  }
+}
+
+console.log(sumSalaries(company));
