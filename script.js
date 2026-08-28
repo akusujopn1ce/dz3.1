620e894bc4f13c85cda60bbd944afc1ac645afdf
akusujopn1ce <akusujopnice007@gmail.com>
2026-08-28 18:01:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-const container = document.getElementById('table-container');
+const textBlock = document.getElementById('text-block');
+const button = document.getElementById('color-btn');
 
-const table = document.createElement('table');
-
-for (let i = 1; i <= 10; i++) {
-    const row = document.createElement('tr');
-
-    for (let j = 1; j <= 10; j++) {
-        const isHeader = (i === 1 || j === 1);
-        
-        const cell = document.createElement(isHeader ? 'th' : 'td');
-        
-        cell.textContent = i * j;
-
-        row.appendChild(cell);
-    }
-    
-    table.appendChild(row);
-}
-
-container.appendChild(table);
+button.addEventListener('click', function() {
+    textBlock.classList.toggle('text-highlight');
+});
